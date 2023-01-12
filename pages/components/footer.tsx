e8055ee0864ab16ce4fc643f287124b355fae67c
chuck <chuck.qukun@gmail.com>
2023-01-12 20:00:32 +0800

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -3,20 +3,6 @@ import {Text, Container, Link, Row} from "@nextui-org/react";
 export default function Footer() {
     return (
         <Container css={{mt: "$12", mb: "$10"}} color={"#fff"}>
-            <Row justify="center">
-                Created by&#20;
-                <Text b>
-                    <Link href="https://forecho.com" target="_blank" rel="noopener noreferrer">
-                        forecho
-                    </Link>
-                </Text>
-                &#20;on&#20;
-                <Text b>
-                    <Link href="https://github.com/forecho/cherries" target="_blank" rel="noopener noreferrer">
-                        Github
-                    </Link>
-                </Text>
-            </Row>
         </Container>
     );
 };
